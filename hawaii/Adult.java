d7506115d1b32652b965034c5cd190b27c781b49
Ruimin Cai <cairuimin1215@gmail.com>
2012-11-04 15:59:56 -0500

    project 1

---
+++ hawaii/Adult.java
@@ -0,0 +1,62 @@
+package hawaii;
+
+/**
+ * Provides the behavior for a single adult.
+ *
+ */
+public class Adult implements Runnable {
+	// Is the adult on Oahu?
+	private boolean onOahu = true;
+	
+	// The boat the adult wants to get into
+	private Boat boat;
+	
+	// The information about where everyone is
+	private Hawaii hawaii;
+	
+	// This adult's id
+	private int id;
+	
+	public Adult (Boat boat, Hawaii hawaii, int id) {
+		this.boat = boat;
+		this.hawaii = hawaii;
+		this.id = id;
+	}
+	
+	public void run() {
+		try {
+			// Adult is done once it reaches Molokai
+			while (onOahu) {
+				
+				// Lock the boat
+				synchronized(boat) {
+					// Wait for the boat to be on Molokai
+					if (boat.isOnMolokai()) {
+						boat.wait();
+					}
+					
+					// Get in the boat if it is empty and there is at least one child on Molokai who
+					// will be able to row back or if there are no children on Oahu.  Note that this
+					// last situation can only occur if there are no children in the original problem.
+					if (boat.isOnOahu() && boat.isEmpty() 
+							&& (hawaii.numChildrenOnMolokai() >= 1 || hawaii.numChildrenOnOahu() == 0)) {
+						// Get in the boat
+						boat.addAdult();
+						System.out.println("Adult " + id + " is rowing to Molokai");
+						onOahu = false;
+						
+						// Go to Molokai
+						hawaii.addAdultToMolokai();
+						boat.gotoMolokai();
+						
+						// Get out of the boat
+						boat.removeAdult();
+					}
+				}
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ hawaii/Hawaii.java
@@ -0,0 +1,59 @@
+package hawaii;
+
+/**
+ * Tracks the number of people on each island.
+ * Provides the main method for the program
+ *
+ */
+public class Hawaii {
+	private static final int NUM_CHILDREN = 3;
+	private static final int NUM_ADULTS = 5;
+	
+	private int numOnOahu = NUM_CHILDREN + NUM_ADULTS;
+	private int numChildrenOnOahu = NUM_CHILDREN;
+
+	public int numPeopleOnOahu() {
+		return numOnOahu;
+	}
+
+	public int numChildrenOnOahu() {
+		return numChildrenOnOahu;
+	}
+
+	public int numChildrenOnMolokai() {
+		return NUM_CHILDREN - numChildrenOnOahu;
+	}
+
+	public void addChildToMolokai() {
+		assert numChildrenOnOahu > 0;
+		numChildrenOnOahu--;
+		numOnOahu--;
+	}
+
+	public void addChildToOahu() {
+		assert numChildrenOnOahu < NUM_CHILDREN;
+		numChildrenOnOahu++;
+		numOnOahu++;
+	}
+	
+	public void addAdultToMolokai() {
+		assert numOnOahu > 0;
+		numOnOahu--;
+	}
+
+	public static void main(String[] args) {
+		Boat boat = new Boat();
+		Hawaii hawaii = new Hawaii();
+		
+		for (int i = 0; i < NUM_CHILDREN; i++) {
+			new Thread(new Child(boat, hawaii, i)).start();
+		}
+
+		for (int i = 0; i < NUM_ADULTS; i++) {
+			new Thread(new Adult(boat, hawaii, i)).start();
+		}
+
+	}
+
+
+}
